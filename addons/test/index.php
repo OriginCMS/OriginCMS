5a0148561f99f3acb65a821d87d5d00e4e13c229
Cydrobolt <Cydrobolt@users.noreply.github.com>
2014-03-06 16:47:09 -0500

    Update index.php

--- addons/test/index.php
+++ addons/test/index.php
@@ -3,7 +3,7 @@
 	$info['name']='Addon template';
 	$info['version']='1.0.0';
 	$info['description']='This is a simple plugin to show how plugins work';
-	$credits['author']='gradymcd';
+	$credits['author']='AuthorName';
 
 
 	echo 'Look at my amazing addon!';
